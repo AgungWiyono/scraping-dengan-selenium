e4802f3f9f7b55c8acb614cd4f067335fa0457d1
Agung Wiyono <wiyonoagung1@gmail.com>
2022-01-04 08:38:19 +0700

    Add lokasi, filter double and write to excel

--- scraper.py
+++ scraper.py
@@ -1,12 +1,15 @@
+import sys
 import os
 import json
 import time
 import random
 import logging
 
-from csv import DictWriter
 from typing import Any, Dict, List, Union
 from urllib.parse import urlencode
+from logging.handlers import RotatingFileHandler
+
+import tablib
 
 from selenium import webdriver
 from requests_html import HTMLSession
@@ -14,15 +17,21 @@ from requests_html import HTMLSession
 if not os.path.exists("logs"):
     os.mkdir("logs")
 logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s %(levelname)-8s %(message)s",
-    filename="logs/scraper.log",
-    filemode="w",
+file_handler = RotatingFileHandler(
+    "./logs/scraper.log",
+    maxBytes=1024 * 5,
+    backupCount=10,
 )
+logging.basicConfig(level=logging.INFO)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(
+    logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+)
+logger.addHandler(file_handler)
 
 
 def get_page_data(url: str) -> Union[Dict[str, Any], None]:
+    print(f"Hitting {url}")
     logger.info(f"Hitting {url}")
 
     session = HTMLSession()
@@ -44,10 +53,10 @@ def get_page_data(url: str) -> Union[Dict[str, Any], None]:
     return jsondata["data"]
 
 
-def id_getter(keyword: str, page=0) -> List[str]:
+def id_getter(keyword: str, page=0, location=None) -> List[str]:
     parameters = {
         "facet_limit": 100,
-        "location": 1000001,
+        "location": location if location else 1000001,
         "location_facet_limit": 40,
         "query": keyword,
         "spellcheck": True,
@@ -107,10 +116,45 @@ def userdata_getter(
                 "phone": userdata["phone"],
                 "verified": userdata["verification_status"],
                 "about": userdata["about"],
-                "url": url,
+                "url": url[12:],
             }
         )
     return tempdata
+
+
+def find_location(keyword) -> str:
+    parameters = {
+        "input": keyword,
+        "limit": 50,
+    }
+    url = "https://www.olx.co.id/api/locations/autocomplete?"
+    querystring = urlencode(parameters)
+    browser = HTMLSession()
+    resp = browser.get(f"{url}{querystring}")
+    data = resp.json()["data"]["suggestions"]
+
+    suggested_location = []
+    valid_location = []
+    for item in data:
+        choice = {
+            "nama": item["name"],
+            "jenis": item["type"],
+            "id": item["id"],
+        }
+        valid_location.append(choice["id"])
+        suggested_location.append(choice)
+    print(f"Pilihan lokasi yang tersedia berdasarkan keyword {keyword}")
+    for item in suggested_location:
+        print(
+            f"ID: {item['id']}, Nama: {item['nama']}, Jenis: {item['jenis']}"
+        )
+    location_id = input("Masukkan id dari lokasi yang diinginkan: ")
+    print(valid_location)
+    if int(location_id) not in valid_location:
+        print("Lokasi yang dipilih tidak valid.")
+        print("Menghentikan program...")
+        sys.exit()
+    return location_id
 
 
 def scrape_data():
@@ -126,6 +170,13 @@ def scrape_data():
         or 0
     )
 
+    location_keyword = input(
+        "Masukkan lokasi yang diinginkan (default kosong): "
+    )
+    location_id = None
+    if location_keyword:
+        location_id = find_location(location_keyword)
+
     print("Browser akan terbuka.")
     print("Silakan login di browser kemudian kembali ke console")
     browser = webdriver.Firefox()
@@ -136,36 +187,48 @@ def scrape_data():
         return
 
     page = startpage
-    userdata = []
-    while True:
+    headers = ["name", "phone", "verified", "about", "url"]
+    userdata = tablib.Dataset()
+    userdata.headers = headers
+    userphone_exists = []
+
+    state = True
+    while state:
         if endpage > 0:
             if page > endpage:
                 logger.info("Ending Scraping Process")
                 break
 
         try:
-            userids = id_getter(keyword, page)
+            userids = id_getter(keyword, page, location_id)
         except Exception as e:
             print(f"Gagal mengambil data untuk halaman {page}")
             print(e)
             browser.close()
-            return
+            state = False
+
+        if not state:
+            break
 
         try:
             usertempdata = userdata_getter(browser, userids)
+        except KeyboardInterrupt:
+            print("Proses dihentikan")
         except Exception as e:
             print(f"Gagal mengambil data untuk user pada halaman {page}")
             print(e)
             browser.close()
+            state = False
 
-        userdata.extend(usertempdata)
+        for row in usertempdata:
+            if row["phone"] not in userphone_exists:
+                userphone_exists.append(row["phone"])
+                userdata.append(row.values())
         page += 1
 
-    filename = f"result_page_{startpage}_to_{page-1}.csv"
-    with open(filename, "w") as f:
-        writer = DictWriter(f, userdata[0].keys())
-        writer.writeheader()
-        writer.writerows(userdata)
+    filename = f"result_page_{startpage}_to_{page-1}.xlsx"
+    with open(filename, "wb") as f:
+        f.write(userdata.export("xlsx"))
     browser.close()
 
 
